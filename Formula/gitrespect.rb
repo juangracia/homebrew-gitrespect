90a44703bb7f083bb99cffebd29fe00891bf7d40
Juan Gracia <juanmgracia@gmail.com>
2026-01-03 02:56:47 -0300

    feat: update to v0.2.0 with recursive scanning

--- Formula/gitrespect.rb
+++ Formula/gitrespect.rb
@@ -1,26 +1,26 @@
 class Gitrespect < Formula
   desc "Respect your git work with real metrics - developer productivity CLI"
   homepage "https://github.com/juangracia/gitrespect"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/juangracia/gitrespect/releases/download/v0.1.0/gitrespect-darwin-arm64.tar.gz"
-      sha256 "31a8753f912ff9df5ed246ff086d4f632aab7088cd558145956cf47b0f8bd3b9"
+      url "https://github.com/juangracia/gitrespect/releases/download/v0.2.0/gitrespect-darwin-arm64.tar.gz"
+      sha256 "35af89ee644628a7dc02d526aea49c9a2dc1564c48d2654fa6edc2c2e6867f08"
     else
-      url "https://github.com/juangracia/gitrespect/releases/download/v0.1.0/gitrespect-darwin-amd64.tar.gz"
-      sha256 "e179c815250c084fd184be947dd2fb722c6fc0b67a6b10043a0e71cad4e80126"
+      url "https://github.com/juangracia/gitrespect/releases/download/v0.2.0/gitrespect-darwin-amd64.tar.gz"
+      sha256 "6f4f6d1a674b6f2457c244cf75b125aec5a7fc995760b581298529aaf2232fc5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/juangracia/gitrespect/releases/download/v0.1.0/gitrespect-linux-arm64.tar.gz"
-      sha256 "9811d8b9c2220142e129983e64aa86762434a932eb020cb01966feb0254959c4"
+      url "https://github.com/juangracia/gitrespect/releases/download/v0.2.0/gitrespect-linux-arm64.tar.gz"
+      sha256 "9b8d66988d9600b3a1272090d43a623a6818e0a9e4d43f305a9246e5c7f838ae"
     else
-      url "https://github.com/juangracia/gitrespect/releases/download/v0.1.0/gitrespect-linux-amd64.tar.gz"
-      sha256 "3d4276b5629847819b711c57836b2e8a17dca9ec4f93c5737567d706a3339614"
+      url "https://github.com/juangracia/gitrespect/releases/download/v0.2.0/gitrespect-linux-amd64.tar.gz"
+      sha256 "4037f34cdaa57425fea9c246741aa2e5a761ef5557f18df254c879d23febaf05"
     end
   end
 
